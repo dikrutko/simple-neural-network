9a8d85290f621e986611b614c8046a741b06a284
dikrutko <82881342+dikrutko@users.noreply.github.com>
2022-12-06 23:07:08 +0700

    Delete

--- 2.py
+++ 2.py
@@ -4,7 +4,9 @@ from PIL import ImageFilter
 
 filename = os.listdir("./Winter")   ##Путь к папке для размещения оригинальных изображений
 
-saple = Image.open('./Winter/elephant 100/IMG_0798.JPG')
+saple = Image.open('./Winter/rock_grandfather 144/IMG_E8791.JPG')
 #saple.show()
-a1 = saple.filter(ImageFilter.SMOOTH)
+#a1 = saple.filter(ImageFilter.SMOOTH)
+a1 = saple.rotate(350); 
 a1.show()
+#a1.save('./Winter')

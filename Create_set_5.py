8957aeb8b100ed894b773fcde297281abf6cc3f9
dikrutko <82881342+dikrutko@users.noreply.github.com>
2023-02-23 19:11:39 +0700

    Add set 5

--- Create_set_5.py
+++ Create_set_5.py
@@ -26,3 +26,19 @@ for obj in filenameTest:
       w,h = im.size
       new_im=im.resize((224,224))
       new_im.save(str_new_dir+"/"+image)
+
+filenameTraining = os.listdir("./Set_4/Training")   ##Путь к папке для размещения оригинальных изображений
+
+for obj in filenameTest:
+   past_dir = os.listdir("./Set_4/Training"+"/"+obj)
+   str_past_dir = "./Set_4/Training/"+obj
+   folder = os.makedirs("./Set_5/Valid/{}".format(obj), exist_ok=True)
+   str_new_dir = "./Set_5/Valid/"+obj
+   k=0
+   for image in past_dir:
+    if k <= 60:
+      im = Image.open(str_past_dir+'/'+image)
+      w,h = im.size
+      new_im=im.resize((224,224))
+      new_im.save(str_new_dir+"/"+image)
+      k=k+1

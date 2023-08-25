9dd84532c36aae78aad93faa910d754e43e2c8da
dikrutko <82881342+dikrutko@users.noreply.github.com>
2023-08-25 22:36:37 +0700

    Change catalogs

--- 3.py
+++
@@ -1,7 +0,0 @@
-import os
-from PIL import Image
-
-saple = Image.open('./Training/fourth_pillar/4.JPG')
-a1 = saple.resize((318,318))
-a1.save('./Training/fourth_pillar/4.JPG')
-#a1.save('./Winter')
